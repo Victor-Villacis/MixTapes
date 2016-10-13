add39d5555878aef7429373d926725be82f55e26
vicrtor villacis <victor.villacis@outlook.com>
2016-10-13 04:27:36 -0400

    server.js

--- server.js
+++ server.js
@@ -29,7 +29,9 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -38,7 +40,7 @@ var router = express.Router();
 
 // home page
 router.get('/', function(req, res) {
-    //res.render('index', { title: 'Express' }); 
+    //res.render('index', { title: 'Express' });
     res.render('./public/js/app/templates/index.html');
 });
 
@@ -47,15 +49,15 @@ router.get('/api/products', function(req, res, next) {
     product_controller.findAll(res);
 });
 router.get('/api/product/:id', function(req, res, next) {
-    product_controller.findByOne(req,res);
+    product_controller.findByOne(req, res);
 });
 router.post('/api/product/save', function(req, res, next) {
-    product_controller.save(req,res);
+    product_controller.save(req, res);
 });
 router.post('/api/comment/save', function(req, res, next) {
     //get ip of client
     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    product_controller.saveComment(req,res, ip);
+    product_controller.saveComment(req, res, ip);
 });
 router.get('/api/comments', function(req, res, next) {
     product_controller.findAll(res);
@@ -67,9 +69,9 @@ app.use('/', router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -77,25 +79,25 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 app.listen(PORT);
-console.log('Listening on port ' + PORT); 
+console.log('Listening on port ' + PORT);
 module.exports = app;
